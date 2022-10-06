0e26228161b8718c681ad4822bb7a4a87e942f0e
Deokhaeng <101077014+Deokhaeng@users.noreply.github.com>
2022-10-06 19:08:23 +0900

    Feat: add city, weather

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+import { WEATHER_API_KEY } from "react-native-dotenv";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
+console.log(WEATHER_API_KEY);
+
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${WEATHER_API_KEY}`
+    );
+    const json = await response.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getWeather();
+  });
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -32,7 +61,6 @@ export default function App() {
           <Text style={styles.description}>Sunny</Text>
         </View>
       </ScrollView>
-      <StatusBar />
     </View>
   );
 }
